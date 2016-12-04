c2ce46ec17503e696ad5171ca0102a4ebd4e47e1
Gogs <gogs@fake.local>
2016-12-04 21:53:46 +0800

    2016-04-12 21:53

--- dealData.py
+++ dealData.py
@@ -6,8 +6,8 @@ team_code="1419"
 
 df = pd.read_csv(team_code + ".csv")
 train, test = df.iloc[1:150,:], df.iloc[150:200,:]
-y_train, X_train = train["funny_rating"], train.drop(["funny_rating", "stars"], axis = 1)
-X_test = test.drop(["funny_rating", "stars"], axis = 1)
+y_train, X_train = train["funny_rating"], train.drop(["funny_rating", "stars", "unkown"], axis = 1)
+X_test = test.drop(["funny_rating", "stars", "unkown"], axis = 1)
 
 model = RandomForestClassifier(n_estimators=50, criterion="gini",max_features="sqrt",min_samples_leaf=1,n_jobs=4)
 model.fit(X_train, y_train)
@@ -15,7 +15,7 @@ model.fit(X_train, y_train)
 predict_tmp = model.predict(X_test)
 test_success = 0
 test_fail = 0
-for j,k in zip(predict_tmp,test["funny_rating"]):
+for j,k in zip(predict_tmp, test["funny_rating"]):
     if j == k:
         test_success = test_success + 1
     else:
