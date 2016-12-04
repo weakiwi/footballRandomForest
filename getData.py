427231713a05d69f71347db276263ff9603c3846
weakiwi <670932228@qq.com>
2016-12-04 19:19:36 +0800

    modify something

--- getData.py
+++ getData.py
@@ -11,7 +11,7 @@ r = requests.get('http://zq.win007.com/jsData/teamInfo/teamDetail/tdl' + team_co
 tmp = r.content
 m = re.findall(r"(\[\d{7}.*.\];)", tmp)[0]
 m = m.split('],[')
-csv_header = ["fouls", "yellow_card", "red_card", "possession_rate", "shots", "on target", "passing", "passing_success", "rating",  "funny_rating"]
+csv_header = ["fouls", "yellow_cards", "red_cards", "possession_rate", "shots", "shot_on target", "passing", "passing_success", "passing_success_rate",  "crossing", "corners", "offside", "unkown", "head", "head_success", "save", "slide_tackle", "shot_out_of target", "blocked_shots", "thorw-in", "stars", "hometeam", "funny_rating"]
 with open(team_code + ".csv", "a+") as f:
     f_csv = csv.writer(f)
     f_csv.writerow(csv_header)
@@ -22,15 +22,18 @@ for i in m:
     with open(team_code + '.csv', 'a+') as f:
         f_csv = csv.writer(f)
         tmp = i.split(",")
-        del tmp[0:10]
-        del tmp[12]
-        tmp = tmp[8:19]
-        tmp = [ float(i) for i in tmp]
+        if tmp[2] == team_code:
+            tmp.append(1)
+        else:
+            tmp.append(0)
+        del tmp[0:8]
+        #tmp = tmp[8:19]
         game_degree = lambda x:abs(int(x[0])-int(x[1]))
         if game_degree(tmp) < 3:
             tmp.append(1)
         else:
             tmp.append(0)
         del tmp[0:2]
+        tmp = [ float(i) for i in tmp]
         f_csv.writerow(tmp)
 
